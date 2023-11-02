63681a92cf34e44bd22ade5f73d01a6d0e88ee27
adityagaur0 <adityakumargaur0@gmail.com>
2023-11-03 04:28:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:imagepicker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ImagePickerDemo(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
